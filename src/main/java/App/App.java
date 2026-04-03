a741073fabaa987eeee7abe5527df5588ad84791
malakBourrich <malak74t@gmail.com>
2026-04-03 09:27:39 +0100

    first commit devops

---
+++ src/main/java/App/App.java
@@ -0,0 +1,13 @@
+package App;
+
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins Pipeline");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
