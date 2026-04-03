1735706540fc91470f5f8a61bd3b655600bd5a19
malakBourrich <malak74t@gmail.com>
2026-04-03 11:20:15 +0100

    test build

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -5,6 +5,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Jenkins Pipeline");
+        System.out.println("Hello malak");
     }
 
     public int add(int a, int b) {
